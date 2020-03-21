4a223c36f6467d9f0faafd8dd4e1461bb0054f28
Benjamin Cabrera <benny@bcabrera.de>
2020-03-21 03:05:17 +0100

    Created scaffold for Angular app and a first page under route /sign-up

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+
+const routes: Routes = [
+	{ path: 'sign-up', component: SignUpComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
